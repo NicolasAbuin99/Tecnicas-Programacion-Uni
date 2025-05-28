adc2e0a8439fd6aeca97c25b31e3e111f375aa43
NicolasAbuin99 <nicolasabuin19@gmail.com>
2025-05-28 12:54:09 -0300

    Add files via upload

---
+++ CocecionariosCoches/GestorCoche.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CocecionariosCoches
+{
+    public class GestorCoche : Icoches
+    {
+        private List<Coches> _coches= new List<Coches> ();
+       
+        public void AgregarLista(string modelo, string color, int precio)
+        {
+            _coches.Add(new Coches(modelo, color, precio));
+        }
+
+        public void ComprarCoches(int indice)
+        {
+            if(indice>=00 && indice<= _coches.Count)
+            {
+                _coches.RemoveAt(indice);
+            }
+        }
+
+        public List<Coches> ObtenerLista()
+        {
+            return _coches;
+        }
+    }
+}
