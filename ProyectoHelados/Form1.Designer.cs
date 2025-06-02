229647248b5131b11c7f0541f0c98f34e59bf9a5
NicolasAbuin99 <nicolasabuin19@gmail.com>
2025-06-01 21:13:49 -0300

    Add files via upload

---
+++ ProyectoHelados/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace ProyectoHelados
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label5 = new Label();
+            txtNombre = new TextBox();
+            cmbTamaño = new ComboBox();
+            btnGuardar = new Button();
+            btnPedidos = new Button();
+            dataGridView1 = new DataGridView();
+            btnTotalRecaudado = new Button();
+            btnSaborMaspedido = new Button();
+            btnTamañoMasPedido = new Button();
+            clbSabores = new CheckedListBox();
+            btnTicket = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(26, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(108, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Compra del helado";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(26, 57);
+            label2.Name = "label2";
+            label2.Size = new Size(107, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Ingrese su Nombre";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(26, 151);
+            label3.Name = "label3";
+            label3.Size = new Size(89, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Ingrese el sabor";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(26, 97);
+            label5.Name = "label5";
+            label5.Size = new Size(101, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Ingrese el tamaño";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(159, 49);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(100, 23);
+            txtNombre.TabIndex = 5;
+            // 
+            // cmbTamaño
+            // 
+            cmbTamaño.FormattingEnabled = true;
+            cmbTamaño.Location = new Point(159, 94);
+            cmbTamaño.Name = "cmbTamaño";
+            cmbTamaño.Size = new Size(121, 23);
+            cmbTamaño.TabIndex = 8;
+            // 
+            // btnGuardar
+            // 
+            btnGuardar.Location = new Point(21, 241);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(106, 23);
+            btnGuardar.TabIndex = 9;
+            btnGuardar.Text = "Guardar Compra";
+            btnGuardar.UseVisualStyleBackColor = true;
+            btnGuardar.Click += btnGuardar_Click;
+            // 
+            // btnPedidos
+            // 
+            btnPedidos.Location = new Point(921, 17);
+            btnPedidos.Name = "btnPedidos";
+            btnPedidos.Size = new Size(100, 23);
+            btnPedidos.TabIndex = 10;
+            btnPedidos.Text = "Lista de pedidos";
+            btnPedidos.UseVisualStyleBackColor = true;
+            btnPedidos.Click += btnPedidos_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(781, 57);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(240, 150);
+            dataGridView1.TabIndex = 11;
+            // 
+            // btnTotalRecaudado
+            // 
+            btnTotalRecaudado.Location = new Point(336, 44);
+            btnTotalRecaudado.Name = "btnTotalRecaudado";
+            btnTotalRecaudado.Size = new Size(99, 23);
+            btnTotalRecaudado.TabIndex = 12;
+            btnTotalRecaudado.Text = "TotalRecaudado";
+            btnTotalRecaudado.UseVisualStyleBackColor = true;
+            btnTotalRecaudado.Click += btnTotalRecaudado_Click;
+            // 
+            // btnSaborMaspedido
+            // 
+            btnSaborMaspedido.Location = new Point(477, 44);
+            btnSaborMaspedido.Name = "btnSaborMaspedido";
+            btnSaborMaspedido.Size = new Size(111, 23);
+            btnSaborMaspedido.TabIndex = 13;
+            btnSaborMaspedido.Text = "Sabor mas pedido";
+            btnSaborMaspedido.UseVisualStyleBackColor = true;
+            btnSaborMaspedido.Click += btnSaborMaspedido_Click;
+            // 
+            // btnTamañoMasPedido
+            // 
+            btnTamañoMasPedido.Location = new Point(625, 44);
+            btnTamañoMasPedido.Name = "btnTamañoMasPedido";
+            btnTamañoMasPedido.Size = new Size(128, 23);
+            btnTamañoMasPedido.TabIndex = 14;
+            btnTamañoMasPedido.Text = "Tamaño mas vendido";
+            btnTamañoMasPedido.UseVisualStyleBackColor = true;
+            btnTamañoMasPedido.Click += btnTamañoMasPedido_Click;
+            // 
+            // clbSabores
+            // 
+            clbSabores.FormattingEnabled = true;
+            clbSabores.Location = new Point(159, 140);
+            clbSabores.Name = "clbSabores";
+            clbSabores.Size = new Size(120, 94);
+            clbSabores.TabIndex = 16;
+            // 
+            // btnTicket
+            // 
+            btnTicket.Location = new Point(477, 113);
+            btnTicket.Name = "btnTicket";
+            btnTicket.Size = new Size(103, 23);
+            btnTicket.TabIndex = 17;
+            btnTicket.Text = "Promedio Ticket";
+            btnTicket.UseVisualStyleBackColor = true;
+            btnTicket.Click += btnTicket_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1043, 450);
+            Controls.Add(btnTicket);
+            Controls.Add(clbSabores);
+            Controls.Add(btnTamañoMasPedido);
+            Controls.Add(btnSaborMaspedido);
+            Controls.Add(btnTotalRecaudado);
+            Controls.Add(dataGridView1);
+            Controls.Add(btnPedidos);
+            Controls.Add(btnGuardar);
+            Controls.Add(cmbTamaño);
+            Controls.Add(txtNombre);
+            Controls.Add(label5);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label5;
+        private TextBox txtNombre;
+        private ComboBox cmbTamaño;
+        private Button btnGuardar;
+        private Button btnPedidos;
+        private DataGridView dataGridView1;
+        private Button btnTotalRecaudado;
+        private Button btnSaborMaspedido;
+        private Button btnTamañoMasPedido;
+        private CheckedListBox clbSabores;
+        private Button btnTicket;
+    }
+}
---
+++ ProyectoHelados/Form1.cs
@@ -0,0 +1,159 @@
+using Heladeria;
+using System.Collections.Generic; 
+using System.Windows.Forms; 
+namespace ProyectoHelados
+{
+    public partial class Form1 : Form
+    {
+        MiHeladeria miHeladeria = new MiHeladeria();
+        List<DetalleVenta> pedidoActual = new List<DetalleVenta>();
+        public List<string> ListaSabores = new List<string>(); 
+
+        public Form1()
+        {
+            InitializeComponent();
+            CargarTipoTamaños();
+            miHeladeria.CargarSaboresIniciales();
+            CargarSabores();
+        }
+
+        private void CargarTipoTamaños()
+        {
+            cmbTamaño.Items.Add(new DosCincuentaGramos("250 Gramos", 200));
+            cmbTamaño.Items.Add(new MedioKilo("Medio Kilo", 350));
+            cmbTamaño.Items.Add(new Kilo("Kilos", 500));
+            cmbTamaño.DisplayMember = "Tamaño";
+        }
+        
+        private void CargarSabores()
+        {
+            clbSabores.Items.Clear(); 
+            foreach (var sabor in miHeladeria.ListaSabores)
+            {
+                clbSabores.Items.Add(sabor);
+            }
+        }
+
+        public void MostrarPedidos()
+        {
+            try
+            {
+                List<Ventas> todaslasventas = miHeladeria.ObtenerTodasLasVentas();
+                // Limpiar el DataGridView
+                 dataGridView1.Rows.Clear();
+       
+                dataGridView1.Columns.Add("Cliente", "Cliente");
+                dataGridView1.Columns.Add("Fecha", "Fecha");
+                dataGridView1.Columns.Add("Ticket", "Ticket");
+                dataGridView1.Columns.Add("Tamaños", "Tamaños");
+                dataGridView1.Columns.Add("Sabores", "Sabores");
+                dataGridView1.Columns.Add("Total", "Total");
+
+                foreach (Ventas venta in todaslasventas)
+                {
+                    string tamaño = "";
+                    string sabores = "";
+                    double totalventa = 0;
+
+                    foreach (DetalleVenta detalle in venta.DetallesVentas)
+                    {
+                        tamaño += detalle.TipoTamaños.Tamaño + ", ";
+                        sabores += detalle.Helados.Sabor + ", ";
+                        totalventa += detalle.PrecioTotal();
+                    }
+                    dataGridView1.Rows.Add(venta.NombreCliente, venta.FechaVenta, venta.Ticket, tamaño, sabores, totalventa);
+                    //Agrega una nueva fila al DataGridView con los datos que has calculado para cada venta
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error al mostrar pedidios");
+            }
+        }
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                string nombre = txtNombre.Text;
+
+                TipoTamaño tamañoSeleccionado = (TipoTamaño)cmbTamaño.SelectedItem;
+            
+                Ventas nuevaVenta = miHeladeria.RegistrarVenta(nombre);
+
+                foreach (string SaborElegido in clbSabores.CheckedItems)
+                {
+                    Helado helado = new Helado(tamañoSeleccionado, SaborElegido, 1000);
+                   // miHeladeria.AgregarHelado(tamañoSeleccionado, SaborElegido, 1000);
+                    DetalleVenta detalleVenta = new DetalleVenta() { Helados = helado, TipoTamaños = tamañoSeleccionado };
+                    nuevaVenta.DetallesVentas.Add(detalleVenta);
+                    //miHeladeria.AgregarDetalleVenta(nuevaVenta, helado, tamañoSeleccionado);
+                }
+                miHeladeria.ListaVentas.Add(nuevaVenta);
+                MessageBox.Show("Guardado");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error, no se guardo");
+            }
+        }
+        
+        private void btnPedidos_Click(object sender, EventArgs e)
+        {
+            MostrarPedidos();
+        }
+
+        private void btnTotalRecaudado_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double TotalReacudado = miHeladeria.TotalRecaudado();
+                MessageBox.Show("Total recaudado: "+ TotalReacudado);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No hay nada recaudado");
+            }
+        }
+
+        private void btnSaborMaspedido_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string SaborMasPedido = miHeladeria.SaborMasPedidio();
+                MessageBox.Show("El sabor mas pedido es " + SaborMasPedido);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se pudo sacar el sabor mas pedido porque no hubo compras");
+            }
+        }
+
+        private void btnTamañoMasPedido_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string TamañoMasPedido = miHeladeria.TamañoMasPedido();
+                MessageBox.Show("El tamaño mas vendido es " + TamañoMasPedido);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se pudo sacar el tamaño mas pedido porque no hubo compras");
+            }
+        }
+
+        private void btnTicket_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double PromedioTicket = miHeladeria.ticketPromedio();
+                MessageBox.Show("El promedio es " + PromedioTicket);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se pudo sacar el promedio del ticket mas pedido porque no hubo compras");
+            } 
+        }
+    }
+}
